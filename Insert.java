0c43793f343b706f9a2027a9fee0c0eafcd570c3
Sajal17 <sajalmondal1730@gmail.com>
2025-08-16 22:55:16 +0530

    Update Insert.java

--- Insert.java
+++ Insert.java
@@ -10,10 +10,10 @@ import org.hibernate.cfg.Configuration;
 public class Main {
     public static void main(String[] args) {
 
-       /* Student s=new Student();
+        Student s=new Student();
         s.setSid(104);
         s.setName("Mina");
-        s.setSub("Biology"); */
+        s.setSub("Biology");
 
         SessionFactory factory = new Configuration()
                 .configure("hibernate.cfg.xml")
@@ -22,32 +22,12 @@ public class Main {
         Session session=factory.openSession();
 
 
-        // create a data
-
-        // Transaction transaction=session.beginTransaction();
-       // session.persist(s);
-        //transaction.commit();
-
-        // fetch the data
-
-       // Student s1= session.find(Student.class,102);  //eager
-      //  Student s2=session.byId(Student.class).load(103);
-      //  Student s3=session.byId(Student.class).getReference(103); //lazy fetching
-      //  System.out.println(s2);
-
-      // Update data
-
-     // Transaction transaction= session.beginTransaction();
-     // session.merge(s);
-     // transaction.commit();
-
-      // delete data
+        // create a data or insert data
 
         Transaction transaction=session.beginTransaction();
-
-        Student s=session.find(Student.class,104);
-        session.remove(s);
+        session.persist(s);
         transaction.commit();
+
         session.close();
         factory.close();
 
